0105aea8b16a19fa8c9ce81269825c03e3705de5
Abdullah Al Noman Jibon <jibonsorker3@gmail.com>
2025-04-11 13:37:25 -0700

    Add files via upload

---
+++ WT/Function.js
@@ -0,0 +1,32 @@
+function calculate() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const operation = document.getElementById("operation").value;
+    let result;
+
+    switch(operation) {
+      case "+":
+         result = num1 + num2;
+      break;
+      case "-":
+        result = num1 - num2;
+      break;
+      case "*":
+        result = num1 * num2;
+      break;
+      case "/":
+        result = num2 !== 0 ? num1 / num2 : "Cannot divide by zero";
+      break;
+                    
+      case "%":
+        result = num1 % num2;
+      break;
+      case "^":
+        result = Math.pow(num1, num2);
+      break;
+      default:
+        result = "Invalid operation";}
+
+     document.getElementById("result").innerHTML = `Result: ${result}`;
+ }
+   
